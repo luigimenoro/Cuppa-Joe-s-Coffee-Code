ce2fb44a5092de91a930b97b8d456ed41b64f4cd
luigimenoro <Luigi.menoro@liston.school.nz>
2022-09-17 13:15:02 +1200

    17/07/22
    Added a new variable called pickup where if the button found in the delivery form is clicked it will assign the  variable to false. However if the button found within the pickup form is clicked it will assign the variable named "pickup" to true.
    
    This is done so that I can use this variable in the future especially in the menu function when displaying the user's information and the total.

--- main.js
+++ main.js
@@ -2,18 +2,20 @@
 if (window.location.href.includes("/order-information.html")) {
   // Specify the varaible of the form
   let deliveryForm = document.getElementById("deliveryForm");
-
+  // Listens to whenever the user clicks the button within the delivery form
   deliveryForm.addEventListener("submit", function (e) {
     e.preventDefault();
     // Get the values that the user has entered within the text inputs
     let nameEntered = document.getElementById("name").value;
     let addressEntered = document.getElementById("address").value;
     let phoneNumEnt = document.getElementById("phoneNumber").value;
-
+    // To identify if the user wanted the order to be pickup
+    let pickup = false;
     // Place the varaibles or the entered values to the session storage
     sessionStorage.setItem("name", nameEntered);
     sessionStorage.setItem("address", addressEntered);
     sessionStorage.setItem("phone number", phoneNumEnt);
+    sessionStorage.setItem("pickup", pickup);
   });
 }
 
@@ -21,12 +23,15 @@ if (window.location.href.includes("/order-information.html")) {
 if (window.location.href.includes("/pickup-information.html")) {
   // Specify the variable of the form
   let pickupForm = document.getElementById("pickupForm");
-
+  // Listens to whenever the user clicks the button within the pickup form
   pickupForm.addEventListener("submit", function (e) {
     e.preventDefault();
     // Get the values that the user has entered within the text inputs
     let nameEntered1 = document.getElementById("name").value;
+    // To identify if the user wanted the order to be pickup
+    let pickup = true;
     // Place the varaibles or the entered values to the session storage
     sessionStorage.setItem("name", nameEntered1);
+    sessionStorage.setItem("pickup", pickup);
   });
 }
