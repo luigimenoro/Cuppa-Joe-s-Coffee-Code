ba85e6914857e2315877a94bce66db3dc08df317
luigimenoro <Luigi.menoro@liston.school.nz>
2022-09-18 20:47:18 +1200

    17/09/22
    Added function that shows the products for sale.  On this I have created an associative array, that consists of the price for different coffees for sale.
    
    I also have added some styling to the menu order page, by using CSS.

--- main.js
+++ main.js
@@ -1,3 +1,134 @@
+// Assign the constants that will be used such the price and the delivery price
+const PRICE = 5.5;
+const DELIVERYPRICE = 5;
+// Assign coffee products
+var PRODUCT = {
+  1: {
+    name: "Cappuccino",
+    regularPrice: PRICE,
+    mediumPrice: PRICE + 1,
+    largePrice: PRICE + 2,
+  },
+  2: {
+    name: "Flat White",
+    regularPrice: PRICE,
+    mediumPrice: PRICE + 1,
+    largePrice: PRICE + 2,
+  },
+  3: {
+    name: "Hot Mocha",
+    regularPrice: PRICE,
+    mediumPrice: PRICE + 1,
+    largePrice: PRICE + 2,
+  },
+  4: {
+    name: "Café Latte",
+    regularPrice: PRICE,
+    mediumPrice: PRICE + 1,
+    largePrice: PRICE + 2,
+  },
+  5: {
+    name: "Café Latte",
+    regularPrice: PRICE,
+    mediumPrice: PRICE + 1,
+    largePrice: PRICE + 2,
+  },
+  6: {
+    name: "White Chocolate Mocha",
+    regularPrice: PRICE,
+    mediumPrice: PRICE + 1,
+    largePrice: PRICE + 2,
+  },
+  7: {
+    name: "Chai Latte",
+    regularPrice: PRICE,
+    mediumPrice: PRICE + 1,
+    largePrice: PRICE + 2,
+  },
+  8: {
+    name: "Vanilla Latte",
+    regularPrice: PRICE,
+    mediumPrice: PRICE + 1,
+    largePrice: PRICE + 2,
+  },
+  9: {
+    name: "Long Black",
+    regularPrice: PRICE,
+    mediumPrice: PRICE + 1,
+    largePrice: PRICE + 2,
+  },
+  10: {
+    name: "Caramel Latte",
+    regularPrice: PRICE,
+    mediumPrice: PRICE + 1,
+    largePrice: PRICE + 2,
+  },
+  11: {
+    name: "Caramel Latte",
+    regularPrice: PRICE,
+    mediumPrice: PRICE + 1,
+    largePrice: PRICE + 2,
+  },
+  12: {
+    name: "Piccolo Latte",
+    regularPrice: PRICE,
+    mediumPrice: PRICE + 1,
+    largePrice: PRICE + 2,
+  },
+};
+
+// To identify the length of the array named products as it is a associative array, as simple .length function won't work
+let length = function (array) {
+  let size = 0;
+  // Traversing the array
+  for (let key in array) {
+    // Checking if key is present
+    // in arrays or not
+    if (array.hasOwnProperty(key)) {
+      size++;
+    }
+  }
+
+  // Return the size
+  return size;
+};
+
+// Function to show the the products or the coffees that are for sale
+function showMenu() {
+  let menuText = "";
+  let menuContainer = document.getElementById("menuContainer"); // Get the container where the menu is going to be placed in th HTML file
+  // Loops through the length of the product array and adds the items within the product array to the "container" or to the HTML file
+  for (i = 1; i < length(PRODUCT) + 1; i++) {
+    menuText += `
+            <div class="products">
+              <h2 class="product-name">${PRODUCT[i].name}</h2> 
+              <div class="price-products">
+                <p class="price">$${PRODUCT[i].regularPrice.toFixed(2)}</p>
+                <div class="choose-size">
+                <label for="coffeeSize">Choose your coffee size</label>
+                <select name="coffee" id="coffeeSize">
+                <option value="regular">Regular -- $${PRODUCT[i].regularPrice.toFixed(2)}</option>
+                <option value="medium">Medium -- $${PRODUCT[i].mediumPrice.toFixed(2)}</option>
+                <option value="large">Large --$${PRODUCT[i].largePrice.toFixed(2)}</option>
+                </select>
+                </div>
+                <div class="amountProduct flex">
+                <select name="amount" id="amountProducts">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                </select>
+                <button class="btn shop-item-btn" type="button">ADD</button>
+                </div>
+                </div>
+            </div>
+    `;
+  }
+  menuContainer.innerHTML = menuText; //Inserts the menu text string to menu container
+}
+
 // Check if the user is on the order-information screen - (order-information.html)
 if (window.location.href.includes("/order-information.html")) {
   // Specify the varaible of the form
@@ -16,6 +147,9 @@ if (window.location.href.includes("/order-information.html")) {
     sessionStorage.setItem("address", addressEntered);
     sessionStorage.setItem("phone number", phoneNumEnt);
     sessionStorage.setItem("pickup", pickup);
+
+    // To open the menu.html where the user can choose their coffee
+    window.location.assign("menu.html");
   });
 }
 
@@ -33,5 +167,19 @@ if (window.location.href.includes("/pickup-information.html")) {
     // Place the varaibles or the entered values to the session storage
     sessionStorage.setItem("name", nameEntered1);
     sessionStorage.setItem("pickup", pickup);
+
+    // To open the menu.html where the user can choose their coffee
+    window.location.assign("menu.html");
+  });
+}
+
+// Check if the user is on the menu screen - (menu.html)
+if (window.location.href.includes("/menu.html")) {
+  // Specify the button that is going to be clicked when the user is ready to order
+  let startOrder = document.getElementById("startOrder");
+  startOrder.addEventListener("click", function (e) {
+    e.preventDefault();
+    showMenu();
+    startOrder.classList.add("hide");
   });
 }
