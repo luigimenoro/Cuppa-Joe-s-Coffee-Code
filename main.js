e984ce9f30f3541731d92c51c70ecce1a08c3310
luigimenoro <Luigi.menoro@liston.school.nz>
2022-09-19 10:21:53 +1200

    18/08/22
    Added a function that when the user clicks an item and adds it to the shopping cart,  if the user wanted to get rid of that Item, it will remove that item within the shopping cart.

--- main.js
+++ main.js
@@ -28,13 +28,13 @@ var PRODUCT = {
     largePrice: PRICE + 2,
   },
   5: {
-    name: "Café Latte",
+    name: "Macchiato",
     regularPrice: PRICE,
     mediumPrice: PRICE + 1,
     largePrice: PRICE + 2,
   },
   6: {
-    name: "White Chocolate Mocha",
+    name: "White Choc Mocha",
     regularPrice: PRICE,
     mediumPrice: PRICE + 1,
     largePrice: PRICE + 2,
@@ -64,7 +64,7 @@ var PRODUCT = {
     largePrice: PRICE + 2,
   },
   11: {
-    name: "Caramel Latte",
+    name: "Short Black",
     regularPrice: PRICE,
     mediumPrice: PRICE + 1,
     largePrice: PRICE + 2,
@@ -76,6 +76,8 @@ var PRODUCT = {
     largePrice: PRICE + 2,
   },
 };
+
+var numberofCoffees = [];
 
 // To identify the length of the array named products as it is a associative array, as simple .length function won't work
 let length = function (array) {
@@ -93,7 +95,7 @@ let length = function (array) {
   return size;
 };
 
-// Function to show the the products or the coffees that are for sale
+// Function to show the the products or the coffees that are for sale. In this function it loops through the length of the array called products, and the items that are inside the array is then retrieved and placed into a placeholder within a string. And this string is displayed on the HTML when a button is clicked.
 function showMenu() {
   let menuText = "";
   let menuContainer = document.getElementById("menuContainer"); // Get the container where the menu is going to be placed in th HTML file
@@ -106,27 +108,113 @@ function showMenu() {
                 <p class="price">$${PRODUCT[i].regularPrice.toFixed(2)}</p>
                 <div class="choose-size">
                 <label for="coffeeSize">Choose your coffee size</label>
-                <select name="coffee" id="coffeeSize">
-                <option value="regular">Regular -- $${PRODUCT[i].regularPrice.toFixed(2)}</option>
-                <option value="medium">Medium -- $${PRODUCT[i].mediumPrice.toFixed(2)}</option>
-                <option value="large">Large --$${PRODUCT[i].largePrice.toFixed(2)}</option>
+                <select name="coffee" id="coffeeSize1" class="coffee-Size-Chosen">
+                <option value="${PRODUCT[i].regularPrice.toFixed(2)}">Regular -- $${PRODUCT[i].regularPrice.toFixed(2)}</option>
+                <option value="${PRODUCT[i].mediumPrice.toFixed(2)}">Medium -- $${PRODUCT[i].mediumPrice.toFixed(2)}</option>
+                <option value="${PRODUCT[i].largePrice.toFixed(2)}">Large --$${PRODUCT[i].largePrice.toFixed(2)}</option>
                 </select>
                 </div>
                 <div class="amountProduct flex">
-                <select name="amount" id="amountProducts">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                </select>
-                <button class="btn shop-item-btn" type="button">ADD</button>
+                  <select name="amount" id="amountProducts" class="amountChosen">
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  </select>
+                <button class="btn shop-item-btn" type="button" id="shopItem">ADD</button>
                 </div>
                 </div>
             </div>
     `;
   }
   menuContainer.innerHTML = menuText; //Inserts the menu text string to menu container
+}
+
+// Function that will occur when the user is in the menu page -- write more later
+function ready() {
+  // Specifies the buttons that when clicked will remove the item that is present or inside the shopping cart
+  var removeCartBtns = document.getElementsByClassName("btn-danger");
+  for (i = 0; i < removeCartBtns.length; i++) {
+    var button2 = removeCartBtns[i];
+    button2.addEventListener("click", removeCartItem);
+  }
+
+  // Specifies the buttons that when clicked will add the chosen items to the shopping cart
+  var addItemToCartBtns = document.getElementsByClassName("shop-item-btn");
+  for (i = 0; i < addItemToCartBtns.length; i++) {
+    var button = addItemToCartBtns[i];
+    button.addEventListener("click", addItemToCartClicked);
+  }
+}
+// Function that will remove items that is present in the shopping cart - please write more things in here
+function removeCartItem(event) {
+  var btnClicked = event.target;
+  btnClicked.parentElement.parentElement.remove();
+}
+
+// In this function, as I have a select HTML tag, I have to get the value that was chosen in that tag. In this case I need a parameter so that I can identify which select tag I am getting it from and this the "div"
+function getValueFromSelect(div, name) {
+  var selectElement = div.getElementsByClassName(name)[0];
+  return selectElement; // Returns the selectElement
+}
+
+// In this function's job is to get the size that the user has chosen by going through the item, and if the item is the same as the regular price, or the medium price or the large price it wil return that size
+function getSize(item) {
+  if (item == PRODUCT[i].regularPrice.toFixed(2)) {
+    var size = "regular";
+  } else if (item == PRODUCT[i].mediumPrice.toFixed(2)) {
+    var size = "medium";
+  } else {
+    var size = "large";
+  }
+  return size;
+}
+
+function checkValid(userInput, errorText) {
+  if (!userInput || userInput > 10 || userInput < 1) {
+    let errorMessage = document.getElementById(error);
+    errorMessage.innerHTML = errorText;
+    return false;
+  } else {
+    errorText = "";
+    let errorMessage = document.getElementById(error);
+    errorMessage.innerHTML = errorText;
+    return true;
+  }
+}
+// Function to add items to cart
+function addItemToCartClicked(event) {
+  var button = event.target;
+  var shopItem = button.parentElement.parentElement.parentElement;
+  var title = shopItem.getElementsByClassName("product-name")[0].innerText;
+  var selectElement = getValueFromSelect(shopItem, "coffee-Size-Chosen");
+  var price = selectElement.options[selectElement.selectedIndex].value;
+  let getQuantity = getValueFromSelect(shopItem, "amountChosen");
+  let quantity = getQuantity.options[getQuantity.selectedIndex].value;
+  var size = getSize(price);
+  console.log(title, price, size, quantity);
+  addItemtoCart(title, price, size, quantity);
+}
+
+function addItemtoCart(title, price, size, quantity) {
+  var cartRow = document.createElement("div");
+  cartRow.classList.add("cart-row");
+  var cartItems = document.getElementById("shoppingCart");
+  var cartRowContents = `
+      <div class="cart-item cart-column">
+          <span class="cart-item-title">${title}</span>
+      </div>
+      <span class="cart-size">${size}</span>
+      <span class="cart-price cart-column">$${price}</span>
+      <div class="cart-quantity cart-column">
+          <input class="cart-quantity-input" type="number" value="${quantity}">
+          <button class="btn btn-danger" type="button">REMOVE</button>
+      </div>
+`;
+  cartRow.innerHTML = cartRowContents;
+  cartItems.append(cartRow);
+  cartRow.getElementsByClassName("btn-danger")[0].addEventListener("click", removeCartItem);
 }
 
 // Check if the user is on the order-information screen - (order-information.html)
@@ -177,9 +265,26 @@ if (window.location.href.includes("/pickup-information.html")) {
 if (window.location.href.includes("/menu.html")) {
   // Specify the button that is going to be clicked when the user is ready to order
   let startOrder = document.getElementById("startOrder");
+  let askNumber = document.getElementById("askNumber");
+  let shoppingCart = document.getElementById("shoppingCart");
+  let howManyCoffees = document.getElementById("howManyCoffees");
+
   startOrder.addEventListener("click", function (e) {
     e.preventDefault();
     showMenu();
     startOrder.classList.add("hide");
+    shoppingCart.classList.remove("hide");
+    askNumber.classList.add("hide");
+
+    // Check if the entered number of coffees is valid
+    var numOfCoffees = checkValid(howManyCoffees.value, "Please enter an appropriate amount of coffees you are going to buy. Maximum of 10 coffees per order only");
+    // Store the entered number of coffees that the user are buying to an array
+    numberofCoffees.append(numOfCoffees);
+
+    if (document.readyState == "loading") {
+      document.addEventListener("DOMContentLoaded", ready);
+    } else {
+      ready();
+    }
   });
 }
